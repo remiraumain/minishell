0a828b4332b3ee6fa0818621bfc3ea6da6b488c0
Nofy261 <noufy33@yahoo.com>
2025-03-31 16:28:02 +0200

    fixed bug unset

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: nolecler <nolecler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 11:38:39 by nolecler          #+#    #+#             */
-/*   Updated: 2025/03/26 11:22:01 by nolecler         ###   ########.fr       */
+/*   Updated: 2025/03/31 16:23:12 by nolecler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,31 +23,62 @@ static void free_current(t_envp *current)
 	current = NULL;
 }
 
+// int exec_unset(t_cmd *cmd, t_global_data *data)
+// {
+// 	t_envp	*prev;
+// 	t_envp	*current;
+// 	int		i;
+	
+// 	i = 1;
+// 	if (!data->envp)
+// 		return (0);
+// 	while (cmd->argv[i])
+// 	{
+// 		current = data->envp;
+// 		while (current && ft_strcmp(current->name, cmd->argv[i]) != 0)
+// 		{
+// 			prev = current;
+// 			current = current->next;
+// 		}
+// 		if (!current)
+// 			return (0);
+// 		if (current == data->envp)
+// 			data->envp = current->next;
+// 		else
+// 			prev->next = current->next;
+// 		free_current(current);
+// 		i++;
+// 	}
+// 	return (1);// test	
+// }
+
+
 int exec_unset(t_cmd *cmd, t_global_data *data)
 {
-	t_envp	*prev;
-	t_envp	*current;
-	int		i;
-	
-	i = 1;
-	if (!data->envp)
-		return (0);
-	while (cmd->argv[i])
-	{
-		current = data->envp;
-		while (current && ft_strcmp(current->name, cmd->argv[i]) != 0)
-		{
-			prev = current;
-			current = current->next;
-		}
-		if (!current)
-			return (1);
-		if (current == data->envp)
-			data->envp = current->next;
-		else
-			prev->next = current->next;
-		free_current(current);
-		i++;
-	}
-	return (1);// test	
+    t_envp    *prev;
+    t_envp    *current;
+    int        i;
+    
+    i = 1;
+    if (!data->envp)
+        return (0);
+    while (cmd->argv[i])
+    {
+        current = data->envp;
+        while (current && ft_strcmp(current->name, cmd->argv[i]) != 0)
+        {
+            prev = current;
+            current = current->next;
+        }
+        if (current)
+        {
+            if (current == data->envp)
+                data->envp = current->next;
+            else
+                prev->next = current->next;
+            free_current(current);
+        }
+        i++;
+    }
+    return (0);
 }
